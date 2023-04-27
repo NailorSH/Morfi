89af1907b9ba0c2314b6dba1a8279769811d6052
NailorSH <nailshiyatov.04@mail.ru>
2023-04-27 18:13:37 +0300

    Micro changes

--- app/src/main/java/com/nailorsh/morfi/MainActivity.kt
+++ app/src/main/java/com/nailorsh/morfi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nailorsh.morfi
 
 import BigWords
 import Stem
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,6 +13,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -56,10 +58,10 @@ class MainActivity : ComponentActivity() {
 }
 
 fun rootCompare(str1: String, str2: String): Boolean {
-    val d : Int
-    val lenStr1 : Int = str1.length
-    val lenStr2 : Int = str2.length
-    val minLen : Int = min(lenStr1, lenStr2)
+    val d: Int
+    val lenStr1: Int = str1.length
+    val lenStr2: Int = str2.length
+    val minLen: Int = min(lenStr1, lenStr2)
 
     when (minLen) {
         1 -> d = 1
@@ -70,8 +72,8 @@ fun rootCompare(str1: String, str2: String): Boolean {
         else -> d = 6
     }
 
-    for (i in 0..(str1.length-d)) {
-        val subStr = str1.substring(i, i+d)
+    for (i in 0..(str1.length - d)) {
+        val subStr = str1.substring(i, i + d)
         if (str2.contains(subStr)) {
             return true
         }
@@ -79,25 +81,29 @@ fun rootCompare(str1: String, str2: String): Boolean {
     return false
 }
 
-fun LongNonSingleRoot(Words : ArrayList<String>, lang : String = "English", n : Int = 10) : ArrayList<Pair<String, Int>>{
+fun LongNonSingleRoot(
+    Words: ArrayList<String>,
+    lang: String = "English",
+    n: Int = 10
+): ArrayList<Pair<String, Int>> {
     val big = BigWords(Words)
-    val stemmer = Class.forName( lang + "Stemmer").newInstance() as Stem
+    val stemmer = Class.forName(lang + "Stemmer").newInstance() as Stem
     val result = ArrayList<Pair<String, Int>>()
     val resultStem = ArrayList<String>()
     while (result.size != n && big.size() != 0) {
         val buf = big.nextWord()
-        val bufStem = if (buf.length > 4)  stemmer.getStem(buf) else buf
+        val bufStem = if (buf.length > 4) stemmer.getStem(buf) else buf
         var flag = true
         val del = ArrayList<Int>()
         for (i in 0 until resultStem.size)
             if (rootCompare(bufStem, resultStem[i])) {
-                if (!flag && bufStem.length < 6){
+                if (!flag && bufStem.length < 6) {
                     del.add(i)
                 }
                 flag = false
             }
         del.reverse()
-        for (i in del){
+        for (i in del) {
             result.remove(result[i])
             resultStem.remove(resultStem[i])
         }
@@ -107,18 +113,18 @@ fun LongNonSingleRoot(Words : ArrayList<String>, lang : String = "English", n : 
         }
         var len = 0
         var cou = 0
-        while (result.size == n && big.canNextMin() && len < 6 && cou < 1000){
+        while (result.size == n && big.canNextMin() && len < 6 && cou < 1000) {
             val min = stemmer.getStem(big.nextMinWord())
             flag = true
             for (i in 0 until resultStem.size)
                 if (rootCompare(min, resultStem[i])) {
-                    if (!flag && min.length in 4..6){
+                    if (!flag && min.length in 4..6) {
                         del.add(i)
                     }
                     flag = false
                 }
             del.reverse()
-            for (i in del){
+            for (i in del) {
                 result.remove(result[i])
                 resultStem.remove(resultStem[i])
             }
@@ -138,29 +144,9 @@ fun Greeting(context: Context) {
     val languageList = listOf("Русский", "English", "Другие")
     var languageName: String by remember { mutableStateOf(languageList[0]) }
 
+    var resultList by remember { mutableStateOf(ArrayList<Pair<String, Int>>()) }
     var resultText by remember { mutableStateOf("") }
 
-//    var word1 by remember { mutableStateOf("") }
-//    var word2 by remember { mutableStateOf("") }
-//    var word3 by remember { mutableStateOf("") }
-//    var word4 by remember { mutableStateOf("") }
-//    var word5 by remember { mutableStateOf("") }
-//    var word6 by remember { mutableStateOf("") }
-//    var word7 by remember { mutableStateOf("") }
-//    var word8 by remember { mutableStateOf("") }
-//    var word9 by remember { mutableStateOf("") }
-//    var word10 by remember { mutableStateOf("") }
-//
-//    var frequency1 by remember { mutableStateOf("") }
-//    var frequency2 by remember { mutableStateOf("") }
-//    var frequency3 by remember { mutableStateOf("") }
-//    var frequency4 by remember { mutableStateOf("") }
-//    var frequency5 by remember { mutableStateOf("") }
-//    var frequency6 by remember { mutableStateOf("") }
-//    var frequency7 by remember { mutableStateOf("") }
-//    var frequency8 by remember { mutableStateOf("") }
-//    var frequency9 by remember { mutableStateOf("") }
-//    var frequency10 by remember { mutableStateOf("") }
 
     val openFileLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
@@ -176,36 +162,18 @@ fun Greeting(context: Context) {
                         ?.map { it.lowercase(Locale.getDefault()) }
                         ?.toCollection(ArrayList<String>()) ?: emptyList()
 
-                    val languageStemmer = if(languageName == "Русский") "Russian" else languageName
-                    val resultList =
-                        LongNonSingleRoot(words as ArrayList<String>, lang = languageStemmer, n = 10)
-                    val (strings, ints) = resultList.map { (string, int) -> string to int }.unzip()
+                    val languageStemmer = if (languageName == "Русский") "Russian" else languageName
+                    resultList =
+                        LongNonSingleRoot(
+                            words as ArrayList<String>,
+                            lang = languageStemmer,
+                            n = 10
+                        )
+//                    val (strings, ints) = resultList.map { (string, int) -> string to int }.unzip()
 
                     for (i in resultList) {
                         resultText += "${i.first} | ${i.first.length} | ${i.second}\n"
                     }
-
-//                    word1 = strings[0]
-//                    word2 = strings[1]
-//                    word3 = strings[2]
-//                    word4 = strings[3]
-//                    word5 = strings[4]
-//                    word6 = strings[5]
-//                    word7 = strings[6]
-//                    word8 = strings[7]
-//                    word9 = strings[8]
-//                    word10 = strings[9]
-//
-//                    frequency1 = "${ints[0]}"
-//                    frequency2 = "${ints[1]}"
-//                    frequency3 = "${ints[2]}"
-//                    frequency4 = "${ints[3]}"
-//                    frequency5 = "${ints[4]}"
-//                    frequency6 = "${ints[5]}"
-//                    frequency7 = "${ints[6]}"
-//                    frequency8 = "${ints[7]}"
-//                    frequency9 = "${ints[8]}"
-//                    frequency10 = "${ints[9]}"
                 }
 
             }
@@ -324,38 +292,37 @@ fun Greeting(context: Context) {
             )
         }
 
-//        Column(
-//            Modifier
-//                .fillMaxWidth()
-//                .padding(start = 5.dp, end = 5.dp, top = 50.dp, bottom = 10.dp)
-//        ) {
-//            Row(
-//                modifier = Modifier
-//                    .fillMaxWidth(),
-//                horizontalArrangement = Arrangement.SpaceBetween
-//            ) {
-//                Text(
-//                    text = stringResource(R.string.result_words_title),
-//                    fontSize = 18.sp,
-//                    fontWeight = FontWeight.Bold,
-//                    fontStyle = FontStyle.Italic,
-//                    modifier = Modifier
-//                        .weight(3f)
-//                        .padding(start = 10.dp, bottom = 5.dp)
-//                )
-//
-//                Text(
-//                    text = stringResource(R.string.result_frequences_title),
-//                    fontSize = 18.sp,
-//                    fontWeight = FontWeight.Bold,
-//                    fontStyle = FontStyle.Italic,
-//                    modifier = Modifier
-//                        .weight(1f)
-//                        .padding(start = 5.dp, bottom = 5.dp)
-//
-//                )
-//
-//            }
+        Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(start = 5.dp, end = 5.dp, top = 50.dp, bottom = 10.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = stringResource(R.string.result_words_title),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    modifier = Modifier
+                        .weight(3f)
+                        .padding(start = 10.dp, bottom = 5.dp)
+                )
+
+                Text(
+                    text = stringResource(R.string.result_frequences_title),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 5.dp, bottom = 5.dp)
+
+                )
+            }
 
 //            ComposableResultWordsAndFrequency(word1, frequency1)
 //            ComposableResultWordsAndFrequency(word2, frequency2)
@@ -368,36 +335,37 @@ fun Greeting(context: Context) {
 //            ComposableResultWordsAndFrequency(word9, frequency9)
 //            ComposableResultWordsAndFrequency(word10, frequency10)
 
+        }
     }
 }
 
 
-//@Composable
-//fun ComposableResultWordsAndFrequency(word: String, frequency: String) {
-//    Row(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .border(width = 1.dp, color = Color.Gray),
-//        horizontalArrangement = Arrangement.SpaceBetween,
-//        verticalAlignment = Alignment.CenterVertically
-//
-//    ) {
-//        Text(
-//            text = "  $word",
-//            fontStyle = FontStyle.Italic,
-//            modifier = Modifier
-//                .padding(start = 1.dp)
-//                .weight(3f)
-//                .border(width = 1.dp, color = Color.Gray)
-//        )
-//        Text(
-//            text = "  $frequency",
-//            modifier = Modifier
-//                .padding(start = 2.dp)
-//                .weight(1f)
-//        )
-//    }
-//}
+@Composable
+fun ComposableResultWordsAndFrequency(word: String, frequency: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 1.dp, color = Color.Gray),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Text(
+            text = "  $word",
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier
+                .padding(start = 1.dp)
+                .weight(3f)
+                .border(width = 1.dp, color = Color.Gray)
+        )
+        Text(
+            text = "  $frequency",
+            modifier = Modifier
+                .padding(start = 2.dp)
+                .weight(1f)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
