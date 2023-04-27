85651562c0d0cf0a20eb320d2458990a0db8a87f
NailorSH <nailshiyatov.04@mail.ru>
2023-04-27 23:04:09 +0300

    Add scrollable list, FB2 Reader

--- app/src/main/java/com/nailorsh/morfi/MainActivity.kt
+++ app/src/main/java/com/nailorsh/morfi/MainActivity.kt
@@ -2,19 +2,24 @@ package com.nailorsh.morfi
 
 import BigWords
 import Stem
-import android.annotation.SuppressLint
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -24,10 +29,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +48,8 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
 import kotlin.math.min
+import java.io.FileInputStream
+import java.util.zip.ZipInputStream
 
 //import nl.siegmann.epublib.domain.Book
 //import nl.siegmann.epublib.epub.EpubReader
@@ -136,10 +150,21 @@ fun LongNonSingleRoot(
     return result
 }
 
+fun readFB2(context: Context, uri: Uri): String {
+    val inputStream = context.contentResolver.openInputStream(uri)
+    val contents = inputStream?.bufferedReader().use { it?.readText() ?: "" }
+
+    return contents
+        .replace(Regex("<binary.*?</binary>"), "")
+        .replace(Regex("<.*?>"), "")
+}
+
 @Composable
 fun Greeting(context: Context) {
     val image = painterResource(R.drawable.nv_command)
 
+    var timeText by remember { mutableStateOf("Время выполнения программы: ") }
+    var nValue by remember { mutableStateOf(10) }
     var expanded by remember { mutableStateOf(false) }
     val languageList = listOf("Русский", "English", "Другие")
     var languageName: String by remember { mutableStateOf(languageList[0]) }
@@ -147,35 +172,57 @@ fun Greeting(context: Context) {
     var resultList by remember { mutableStateOf(ArrayList<Pair<String, Int>>()) }
     var resultText by remember { mutableStateOf("") }
 
+    val state = rememberLazyListState()
+
 
     val openFileLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
         onResult = { uri ->
             val mimeType: String? = context.contentResolver.getType(uri)
             val inputStream = context.contentResolver.openInputStream(uri)
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream, Charsets.UTF_8))
+
+            var fileText: String?
 
             when (mimeType) {
                 "text/plain" -> {
-                    val fileText = inputStream?.bufferedReader()?.readText()
-                    val words = fileText?.split("[\\W\\d]+".toRegex())
-                        ?.map { it.lowercase(Locale.getDefault()) }
-                        ?.toCollection(ArrayList<String>()) ?: emptyList()
-
-                    val languageStemmer = if (languageName == "Русский") "Russian" else languageName
-                    resultList =
-                        LongNonSingleRoot(
-                            words as ArrayList<String>,
-                            lang = languageStemmer,
-                            n = 10
-                        )
-//                    val (strings, ints) = resultList.map { (string, int) -> string to int }.unzip()
-
-                    for (i in resultList) {
-                        resultText += "${i.first} | ${i.first.length} | ${i.second}\n"
-                    }
+                    val bufferedReader =
+                        BufferedReader(InputStreamReader(inputStream, Charsets.UTF_8))
+                    fileText = inputStream?.bufferedReader()?.readText()
                 }
 
+                else -> {
+                    fileText = readFB2(context, uri)
+//                    val bufferedReader =
+//                        BufferedReader(InputStreamReader(inputStream, Charsets.UTF_8))
+//                    val contents = inputStream?.bufferedReader()?.readText()
+//
+//                    fileText = contents
+//                        ?.replace(Regex("<binary.*?</binary>"), "")
+//                        ?.replace(Regex("<.*?>"), "")
+                }
+            }
+            val words = fileText?.split("[\\p{Punct}\\s]+".toRegex())
+                ?.map { it.lowercase(Locale.getDefault()) }
+                ?.toCollection(ArrayList<String>()) ?: emptyList()
+
+            val languageStemmer = if (languageName == "Русский") "Russian" else languageName
+
+            val startTime = System.currentTimeMillis() // запоминаем текущее время
+            resultList =
+                LongNonSingleRoot(
+                    words as ArrayList<String>,
+                    lang = languageStemmer,
+                    n = nValue
+                )
+            val endTime =
+                System.currentTimeMillis() // запоминаем текущее время после выполнения кода
+            val elapsedTime = endTime - startTime // вычисляем время выполнения в миллисекундах
+            timeText = "Время выполнения программы: $elapsedTime мс"
+
+//                    val (strings, ints) = resultList.map { (string, int) -> string to int }.unzip()
+
+            for (i in resultList) {
+                resultText += "${i.first} | ${i.first.length} | ${i.second}\n"
             }
         }
     )
@@ -191,7 +238,7 @@ fun Greeting(context: Context) {
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .padding(top = 25.dp)
-                .size(200.dp)
+                .size(100.dp)
                 .clip(CircleShape)
                 .border(0.5.dp, Color.Black, CircleShape)
 
@@ -199,7 +246,7 @@ fun Greeting(context: Context) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 10.dp, end = 10.dp, top = 70.dp),
+                .padding(start = 10.dp, end = 10.dp, top = 40.dp),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -262,13 +309,24 @@ fun Greeting(context: Context) {
                     }
                 }
             }
+        }
 
+        nValue = TextFieldWithInputType()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, bottom = 5.dp)
+                .background(Color.Cyan),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = timeText)
         }
 
         Column(
             Modifier
                 .fillMaxWidth()
-                .padding(start = 5.dp, end = 5.dp, top = 50.dp, bottom = 10.dp)
+                .padding(start = 5.dp, end = 5.dp, top = 20.dp, bottom = 10.dp)
         ) {
             Text(
                 text = stringResource(R.string.result_text),
@@ -278,52 +336,68 @@ fun Greeting(context: Context) {
                 modifier = Modifier
                     .padding(start = 10.dp, bottom = 5.dp)
             )
-            Text(
-                text = "(слово | длина | частота)",
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier
-                    .padding(start = 15.dp, bottom = 10.dp)
-            )
 
-            Text(
-                text = resultText,
-                modifier = Modifier
-                    .padding(start = 5.dp, bottom = 10.dp)
-            )
+//            Text(
+//                text = "(слово | длина | частота)",
+//                fontStyle = FontStyle.Italic,
+//                modifier = Modifier
+//                    .padding(start = 15.dp, bottom = 10.dp)
+//            )
+
+//            Text(
+//                text = resultText,
+//                modifier = Modifier
+//                    .padding(start = 5.dp, bottom = 10.dp)
+//            )
         }
 
-        Column(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(start = 5.dp, end = 5.dp, top = 50.dp, bottom = 10.dp)
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = stringResource(R.string.result_words_title),
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                    modifier = Modifier
-                        .weight(3f)
-                        .padding(start = 10.dp, bottom = 5.dp)
-                )
+        Header()
 
-                Text(
-                    text = stringResource(R.string.result_frequences_title),
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(start = 5.dp, bottom = 5.dp)
+        Scaffold {innerPadding ->
+            LazyColumn(contentPadding = innerPadding) {
+                items(resultList) { word ->
+                    WordListItem(word = word.first, frequency = word.second)
 
-                )
+                }
             }
+        }
 
+
+//
+
+
+//        Column(
+//            Modifier
+//                .fillMaxWidth()
+//                .padding(start = 5.dp, end = 5.dp, top = 50.dp, bottom = 10.dp)
+//        ) {
+//            Row(
+//                modifier = Modifier
+//                    .fillMaxWidth(),
+//                horizontalArrangement = Arrangement.SpaceBetween
+//            ) {
+//                Text(
+//                    text = stringResource(R.string.result_words_title),
+//                    fontSize = 18.sp,
+//                    fontWeight = FontWeight.Bold,
+//                    fontStyle = FontStyle.Italic,
+//                    modifier = Modifier
+//                        .weight(3f)
+//                        .padding(start = 10.dp, bottom = 5.dp)
+//                )
+//
+//                Text(
+//                    text = stringResource(R.string.result_frequences_title),
+//                    fontSize = 18.sp,
+//                    fontWeight = FontWeight.Bold,
+//                    fontStyle = FontStyle.Italic,
+//                    modifier = Modifier
+//                        .weight(1f)
+//                        .padding(start = 5.dp, bottom = 5.dp)
+//
+//                )
+//            }
+//
 //            ComposableResultWordsAndFrequency(word1, frequency1)
 //            ComposableResultWordsAndFrequency(word2, frequency2)
 //            ComposableResultWordsAndFrequency(word3, frequency3)
@@ -334,18 +408,65 @@ fun Greeting(context: Context) {
 //            ComposableResultWordsAndFrequency(word8, frequency8)
 //            ComposableResultWordsAndFrequency(word9, frequency9)
 //            ComposableResultWordsAndFrequency(word10, frequency10)
+//
+//        }
+    }
+}
 
+@Composable
+fun Header(
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = MaterialTheme.colors.onSurface.copy(.1f),
+        contentColor = MaterialTheme.colors.primary,
+        modifier = modifier
+            .semantics { heading() }
+            .padding(start = 5.dp, end = 5.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.result_words_title),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                modifier = Modifier
+                    .padding(start = 10.dp, bottom = 5.dp)
+                    .weight(3f)
+            )
+
+            Text(
+                text = stringResource(R.string.result_length_title),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                modifier = Modifier
+                    .padding(start = 10.dp, bottom = 5.dp)
+                    .weight(1f)
+            )
+
+            Text(
+                text = stringResource(R.string.result_frequences_title),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                modifier = Modifier
+                    .padding(end = 5.dp, bottom = 5.dp)
+                    .weight(1f)
+            )
         }
     }
 }
 
-
 @Composable
-fun ComposableResultWordsAndFrequency(word: String, frequency: String) {
+fun WordListItem(word: String, frequency: Int) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .border(width = 1.dp, color = Color.Gray),
+            .padding(8.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
 
@@ -359,17 +480,52 @@ fun ComposableResultWordsAndFrequency(word: String, frequency: String) {
                 .border(width = 1.dp, color = Color.Gray)
         )
         Text(
+            text = "  ${word.length}",
+            modifier = Modifier
+                .weight(1f)
+                .border(width = 1.dp, color = Color.Gray)
+
+        )
+        Text(
             text = "  $frequency",
             modifier = Modifier
-                .padding(start = 2.dp)
                 .weight(1f)
+                .border(width = 1.dp, color = Color.Gray)
         )
     }
 }
 
+@Composable
+fun TextFieldWithInputType(): Int {
+    val localFocusManager = LocalFocusManager.current
+    var textState by remember { mutableStateOf(TextFieldValue()) }
+    var txt by remember { mutableStateOf("10") }
+    TextField(
+        value = textState,
+        onValueChange = {
+            val caretPosition = it.selection
+            val filteredText = it.text.replace(Regex("[^\\d]"), "")
+            textState = TextFieldValue(filteredText, caretPosition)
+            txt = textState.text
+        },
+        label = { Text(text = "Number of output words") },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                localFocusManager.clearFocus()
+                txt = textState.text
+            }
+        )
+    )
+
+    return txt.toInt()
+}
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MorfiTheme {
-    }
+fun PreWordListItem() {
+    WordListItem(word = "555", frequency = 8)
 }
